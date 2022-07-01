89b1861b21f230be6cceebee412c731b0d20cfdd
samwhelp <samwhelp@users.noreply.github.com>
2022-07-01 16:29:10 +0800

    fix

--- db-update.sh
+++ db-update.sh
@@ -2,4 +2,13 @@
 
 cd x86_64
 
-repo-add -n -R archcraft-adjustment.db.tar.gz *.pkg.tar.zst
+#repo-add -n -R archcraft-adjustment.db.tar.gz *.pkg.tar.zst
+
+repo_name="archcraft-adjustment"
+
+rm ${repo_name}.*
+
+repo-add -n -R ${repo_name}.db.tar.gz *.pkg.tar.zst
+
+
+cd "$OLDPWD"
